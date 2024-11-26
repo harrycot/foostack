e945b1597d659fd4984305d042940c767d012d50
Florian Blanc <hvert890@gmail.com>
2024-11-26 13:08:47 +0000

    lock pkg bins

---
+++ scripts/copy-pkg-to-cache.js
@@ -0,0 +1,36 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const crypto = require('node:crypto');
+
+const CONST_DEST_FOLDER = path.join(process.env.HOME, '/.pkg-cache/v3.4');
+
+
+if (!fs.existsSync(path.join(process.env.HOME, '/.pkg-cache'))) { fs.mkdirSync(path.join(process.env.HOME, '/.pkg-cache')) }
+if (!fs.existsSync(path.join(process.env.HOME, '/.pkg-cache/v3.4'))) { fs.mkdirSync(path.join(process.env.HOME, '/.pkg-cache/v3.4')) }
+
+require('./walk').walk(path.join(__dirname, '../pkg-bin'), function(err, results) {
+    if (err) throw err;
+    console.log('\n\n  => Reading files\n');
+
+    results.forEach((file, i) => {
+        if (!file.includes('.sha256sum')) {
+            const _file_sum = fs.readFileSync(`${file}.sha256sum`, 'utf8').split('  ')[0];
+            const _hash = crypto.createHash('sha256');
+            const _input = fs.createReadStream(file);
+            _input.on('readable', () => {
+                const __data = _input.read();
+                if (__data) {
+                    _hash.update(__data);
+                } else {
+                    if (_file_sum === _hash.digest('hex')) {
+                        const _new_filename = file.split('/')[file.split('/').length-1].replace('node-', 'fetched-');
+                        const _new_filepath = path.join(CONST_DEST_FOLDER, _new_filename)
+                        fs.copyFileSync(file, _new_filepath);
+                        console.log(`cp:  ${file}  =>  ${_new_filepath}`);
+                    }
+                }
+            });
+            
+        }
+    });
+});
---
+++ scripts/walk.js
@@ -0,0 +1,25 @@
+const fs = require('node:fs');
+
+exports.walk = (dir, done) => { // https://gist.github.com/h2non/5214201
+    var results = [];
+    fs.readdir(dir, function(err, list) {
+      if (err) return done(err);
+      var i = 0;
+      (function next() {
+        var file = list[i++];
+        if (!file) return done(null, results);
+        file = dir + '/' + file;
+        fs.stat(file, function(err, stat) {
+          if (stat && stat.isDirectory()) {
+            walk(file, function(err, res) {
+              results = results.concat(res);
+              next();
+            });
+          } else {
+            results.push(file);
+            next();
+          }
+        });
+      })();
+    });
+}
