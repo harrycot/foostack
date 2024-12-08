6b2f14a1cff573a32ad99f58f57656ed8b9756f8
Florian Blanc <hvert890@gmail.com>
2024-12-08 14:07:27 +0000

    header subheader footer subfooter: scrollable nav

--- src/web/js/body/ui.js
+++ src/web/js/body/ui.js
@@ -2,12 +2,14 @@
 
 exports.init = () => {
     _asides_panels_toggle();
+    _set_nav_scroll_vertical();
 }
 
-const _asides_panels_toggle_last_class = { left: "", right: "" };
+const _asides_panels_toggle_last_class = {};
 const _asides_panels_toggle = () => {
     for (const pos of ["left", "right"]) {
-        document.querySelector(`body > header > aside.${pos} > button.${pos}-panel-open-toggle`).addEventListener("click", () => {
+        _asides_panels_toggle_last_class[pos] = "";
+        document.querySelector(`body > header > aside.${pos} .${pos}-panel-open-toggle`).addEventListener("click", () => {
             if (document.body.classList.contains(`${pos}-panel-mini-open`) && document.body.classList.contains(`${pos}-panel-open`)) {
                 document.body.classList.remove(`${pos}-panel-open`);
                 _asides_panels_toggle_last_class[pos] = `${pos}-panel-open`;
@@ -23,5 +25,28 @@ const _asides_panels_toggle = () => {
             }
         });
     }
-    
+}
+
+const _set_nav_scroll_vertical = () => {
+    const _horizontal_scrollable_nav = [
+        "header > aside > nav",
+        "header > main > nav",
+        "header > main > nav > section",
+        "footer > aside > nav",
+        "footer > main > nav",
+        "footer > main > nav > section",
+        "main > aside > header > nav",
+        "main > aside > footer > nav",
+        "main > main > header > nav",
+        "main > main > header > nav > section",
+        "main > main > footer > nav",
+        "main > main > footer > nav > section"
+    ];
+    for (const element of document.querySelectorAll(_horizontal_scrollable_nav.join(","))) {
+        element.addEventListener('wheel', (event) => { // https://stackoverflow.com/a/59680347
+            if (!event.deltaY) { return; }
+            event.currentTarget.scrollLeft += event.deltaY + event.deltaX;
+            event.preventDefault();
+        });
+    }
 }
