5e0cc984c818d7cbfaed34ed3843cd6b8b2df512
Florian Blanc <hvert890@gmail.com>
2024-12-09 16:27:02 +0000

    flex boxes and icons base

--- src/web/js/body/ui.js
+++ src/web/js/body/ui.js
@@ -1,52 +1,75 @@
 
 
 exports.init = () => {
-    _asides_panels_toggle();
-    _set_nav_scroll_vertical();
+    // start panels opens to have the scrollable width of elements
+    _document_asides_panels_toggle();
+    _document_bind_vertical_as_horizontal();
+    _document_init_scroll_right();
 }
 
-const _asides_panels_toggle_last_class = {};
-const _asides_panels_toggle = () => {
+const _document_init_scroll_right = (elements_array) => {
+    const _init_scroll_right_elements = elements_array ? elements_array : [
+        "body > header > aside.right > nav",
+        "body > header > main > nav > section:last-child",
+        "body > footer > aside.right > nav",
+        "body > footer > main > nav",
+        "body > footer > main > nav > section:last-child",
+        "body > main > aside.right > header > nav",
+        "body > main > aside.right > footer > nav",
+        "body > main > main > header > nav > section:last-child",
+        "body > main > main > footer > nav > section:last-child",
+    ];
+    for (const element of document.querySelectorAll(_init_scroll_right_elements.join(","))) {
+        element.scrollLeft = element.scrollWidth - element.clientWidth;
+    }
+}
+
+const _document_asides_panels_toggle_last_class = {};
+const _document_asides_panels_toggle = () => {
     for (const pos of ["left", "right"]) {
-        _asides_panels_toggle_last_class[pos] = "";
+        _document_asides_panels_toggle_last_class[pos] = "";
         document.querySelector(`body > header > aside.${pos} .${pos}-panel-open-toggle`).addEventListener("click", () => {
             if (document.body.classList.contains(`${pos}-panel-mini-open`) && document.body.classList.contains(`${pos}-panel-open`)) {
                 document.body.classList.remove(`${pos}-panel-open`);
-                _asides_panels_toggle_last_class[pos] = `${pos}-panel-open`;
+                _document_asides_panels_toggle_last_class[pos] = `${pos}-panel-open`;
             } else if (document.body.classList.contains(`${pos}-panel-mini-open`)) {
-                if (_asides_panels_toggle_last_class[pos] == `${pos}-panel-open`) {
+                if (_document_asides_panels_toggle_last_class[pos] == `${pos}-panel-open`) {
                     document.body.classList.remove(`${pos}-panel-mini-open`);
                 } else {
                     document.body.classList.add(`${pos}-panel-open`);
                 }
             } else {
                 document.body.classList.add(`${pos}-panel-mini-open`);
-                _asides_panels_toggle_last_class[pos] = "";
+                _document_asides_panels_toggle_last_class[pos] = "";
             }
         });
     }
 }
 
-const _set_nav_scroll_vertical = () => {
-    const _horizontal_scrollable_nav = [
-        "header > aside > nav",
-        "header > main > nav",
-        "header > main > nav > section",
-        "footer > aside > nav",
-        "footer > main > nav",
-        "footer > main > nav > section",
-        "main > aside > header > nav",
-        "main > aside > footer > nav",
-        "main > main > header > nav",
-        "main > main > header > nav > section",
-        "main > main > footer > nav",
-        "main > main > footer > nav > section"
+// .scroll-vertical-as-horizontal
+const _document_bind_vertical_as_horizontal = (elements_array) => {
+    const _horizontal_scrollable_nav = elements_array ? elements_array : [
+        "body > header > aside > nav",
+        "body > header > main > nav",
+        "body > header > main > nav > section",
+        "body > footer > aside > nav",
+        "body > footer > main > nav",
+        "body > footer > main > nav > section",
+        "body > main > aside > header > nav",
+        "body > main > aside > footer > nav",
+        "body > main > main > header > nav",
+        "body > main > main > header > nav > section",
+        "body > main > main > footer > nav",
+        "body > main > main > footer > nav > section",
+        ".scroll-vertical-as-horizontal"
     ];
+    const _bind_vertical_as_horizontal = (event) => { // https://stackoverflow.com/a/59680347
+        if (!event.deltaY) { return; }
+        event.currentTarget.scrollLeft += event.deltaY + event.deltaX;
+        event.preventDefault();
+    };
     for (const element of document.querySelectorAll(_horizontal_scrollable_nav.join(","))) {
-        element.addEventListener('wheel', (event) => { // https://stackoverflow.com/a/59680347
-            if (!event.deltaY) { return; }
-            event.currentTarget.scrollLeft += event.deltaY + event.deltaX;
-            event.preventDefault();
-        });
+        element.removeEventListener('wheel', _bind_vertical_as_horizontal);
+        element.addEventListener('wheel', _bind_vertical_as_horizontal);
     }
 }
